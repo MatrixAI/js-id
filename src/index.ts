e6680c65f40d5682d8417e3771bd4046e2d6d2fa
Roger Qiu <roger.qiu@polyhack.io>
2022-01-18 16:35:18 +1100

    Exported Id was meant to be the type Id, and not the class IdInternal

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-export { default as Id } from './Id';
+export type { Id } from './Id';
+export { default as IdInternal } from './Id';
 export { default as IdRandom } from './IdRandom';
 export { default as IdDeterministic } from './IdDeterministic';
 export { default as IdSortable } from './IdSortable';
