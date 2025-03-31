6c8d6d8619c7d54bda90797b6c33720208fd75c1
Brian Botha <brian.botha@matrix.ai>
2025-04-01 09:25:09 +1100

    fix: fixed export `IdSortable` utility functions

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export { default as IdInternal } from './Id';
 export { default as IdRandom } from './IdRandom';
 export { default as IdDeterministic } from './IdDeterministic';
 export { default as IdSortable } from './IdSortable';
+export * as idSortable from './IdSortable';
 export * as utils from './utils';
